e1991c4b59b3411f7d9c2197d081024df6d33577
Aline Yui Taylor <128926302+AlineTaylor@users.noreply.github.com>
2025-01-18 22:10:53 -0600

    Create script.js
    Completed pre-class quick exercise

---
+++ script.js
@@ -0,0 +1,17 @@
+//Quick Exercise
+//Write a program to calculate the discounted price of an item after tax.
+
+//Requirements:
+//1. Declare a variable price and set it to 200.
+let ogPrice = 200;
+//2. Declare variables discountRate (15%) and taxRate (8%).
+let discountRate = 0.15;
+let taxRate = 0.08;
+//3. Calculate the discount and apply it to the price.
+let totalDiscount = ogPrice * discountRate;
+let discountedPrice = ogPrice - totalDiscount;
+//4. Add the tax to the discounted price.
+let totalTax = discountedPrice * taxRate;
+let finalPrice = discountedPrice + totalTax;
+//5. Log the breakdown in the following format:
+console.log(`Original Price: $${ogPrice}, Discount: $${totalDiscount}, Tax: $${totalTax}, Final Price: $${finalPrice}`);
